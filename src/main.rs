9d6f340cc546cc4a6f48a5ac00f213a59e2272c9
Diane Hosfelt <dianehosfelt@gmail.com>
2016-05-25 17:10:10 +0100

    removed stuff

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,12 @@
-#![feature(core_intrinsics)]
 extern crate hyper;
 
-use std::io::Read;
+use std::fs::File;
+use std::error::Error;
+use std::io::prelude::*;
+use std::io::BufWriter;
+use std::path::Path;
 use hyper::{Client};
-//use net_traits::request::{RedirectMode, Referer, Request, RequestMode, ResponseTainting};
 
-
-fn print_type_of<T>(_: &T) -> () {
-    let type_name =
-        unsafe {
-            std::intrinsics::type_name::<T>()
-        };
-    println!("{}", type_name);
-}
-
-/*fn get_resource(url: &str) -> hyper::Result<String> {
-       
-}*/
 
 //http://zsiciarz.github.io/24daysofrust/book/day5.html
 fn main() {
@@ -24,25 +14,34 @@ fn main() {
     let client = Client::new();
     let url = "http://i.imgur.com/PwEwUhA.jpg";
     //let url = "http://zsiciarz.github.io/24daysofrust/book/day5.html";
-
+    
 
     let mut response = match client.get(url).send() {
         Ok(response) => response,
         Err(_) => panic!("Error"),
     };
 
-    print_type_of(&response);
 
-    let mut buf = String::new();
-    let mut buf2 : Vec<u8>;         //TODO hopefully I don't need 2 bufs...
-    match response.read_to_string(&mut buf) {
-        Ok(_) => (),
-        Err(_) => match response.read_to_end(&mut buf2){
-            Ok(_) => (),
-            Err(_) => panic!("I give up."),
-        }
+    let path = Path::new("out/test.out");
+    let display = path.display();
+    let file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", 
+                           display, why.description()),
+        Ok(file) => file,
     };
-    //println!("buf: {}", buf);
+    let mut writer = BufWriter::new(file);
+
+    //let mut buf = String::new();
+    let mut buf = Vec::new();
+    match response.read_to_end(&mut buf){
+        Ok(_) => match writer.write(buf.as_slice()) {
+            Err(why) => panic!("couldn't write to {}: {}", 
+                               display, why.description()),
+            Ok(_) => println!("successfully wrote to {}", display)
+        },
+        Err(why) => panic!("couldn't read response: {}", why.description())
+    }
+
 
 
 }
