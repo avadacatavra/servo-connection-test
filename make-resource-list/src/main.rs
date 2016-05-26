c17581fe7f8812650a5ca70446e4317d8bac5055
Diane Hosfelt <dianehosfelt@gmail.com>
2016-05-26 11:59:53 +0100

    can add resources to a list. not returning right yet

--- make-resource-list/src/main.rs
+++ make-resource-list/src/main.rs
@@ -1,14 +1,64 @@
 extern crate hyper;
 extern crate html5ever;
+#[macro_use] extern crate string_cache;
 
 use hyper::{Client};
 use std::io::{self, Read};
 use std::error::Error;
 use std::string::String;
 
+use std::iter::repeat;
+use std::default::Default;
+use std::mem::replace;
+
 use html5ever::tendril::TendrilSink;
+use html5ever::tendril::StrTendril;
 use html5ever::parse_document;
 use html5ever::rcdom::{Document, Doctype, Text, Comment, Element, RcDom, Handle};
+
+//from https://github.com/servo/html5ever/blob/master/examples/print-rcdom.rs
+//TODO missing lifetime specifier on Vec &str
+fn walk(indent: usize, handle: Handle) -> Vec<String> {
+    let mut resource_list = Vec::new();
+
+    let node = handle.borrow();
+    // FIXME: don't allocate
+    // FIXME: do I really need all of those unused match compares?
+    match node.node {
+        Document => (),
+
+        Doctype(_,_,_) => (),
+
+        Text(_) => (),
+
+        Comment(_) => (),
+
+        Element(ref name, _, ref attrs) => {
+            assert!(name.ns == ns!(html));
+            for attr in attrs.iter() {
+                assert!(attr.name.ns == ns!());
+                //print!(" {}=\"{}\"", attr.name.local, attr.value);
+                if attr.name.local.eq_ignore_ascii_case(&atom!("src")) || 
+                   attr.name.local.eq_ignore_ascii_case(&atom!("href")) {
+                    resource_list.push(replace(&mut attr.value.to_string(), String::new()))
+                }
+            }
+        }
+    }
+
+    for child in node.children.iter() {
+        walk(indent+4, child.clone());
+    }
+
+    for i in resource_list.iter(){
+        println!("{}", i);
+    }
+
+    //TODO it's not returning?
+    resource_list
+}
+
+
 
 fn main() {
     let client = Client::new();
@@ -30,21 +80,14 @@ fn main() {
     
     //So buf holds the html of the site. Now you need to parse through it and grab 
 
-    let stdin = io::stdin();
     let dom = parse_document(RcDom::default(), Default::default()).one(buf);
+    let resource_list = walk(0, dom.document);
    
-
-    let handle = dom.document;
-    let node = handle.borrow();
-    match node.node {
-        Document => (),
-        Doctype(_) => (),
-        Text(ref text) => (),
-        Element(ref name, _,ref attrs) => {
-           // assert!(name.ns == ns!(html));
-            print!("<{}", name.local);
-        }
+    println!("{}", resource_list.len());
+    for i in resource_list.iter(){
+        println!("{}", i);
     }
+    
 
 
 }
