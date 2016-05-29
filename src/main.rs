306132bb8e7d159f7d65f139cf1898745b2b09fa
ddh <dianehosfelt@gmail.com>
2016-05-29 22:25:58 +0100

    added in make resource list code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate hyper;
+extern crate html5ever;
+#[macro_use] extern crate string_cache;
 
 use std::fs::File;
 use std::error::Error;
@@ -6,7 +8,15 @@ use std::io::prelude::*;
 use std::io::BufWriter;
 use std::io::BufReader;
 use std::path::Path;
+
 use hyper::{Client};
+use html5ever::tendril::TendrilSink;
+use html5ever::parse_document;
+use html5ever::rcdom::{Document, Doctype, Text, Comment, Element, RcDom, Handle};
+
+use std::default::Default;
+use std::string::String;
+
 
 //TODO create ./out if dne
 //
@@ -48,8 +58,67 @@ fn fetch_resource(url : &str, client : &Client){
 
 }
 
-fn make_resource_list(url : &str) {
+fn walk(indent: usize, handle: Handle, mut resource_list : &mut Vec<String>)  {
 
+    let node = handle.borrow();
+    // FIXME: don't allocate
+    // FIXME: do I really need all of those unused match compares?
+    match node.node {
+        Document => (),
+
+        Doctype(_,_,_) => (),
+
+        Text(_) => (),
+
+        Comment(_) => (),
+
+        Element(ref name, _, ref attrs) => {
+            assert!(name.ns == ns!(html));
+            for attr in attrs.iter() {
+                assert!(attr.name.ns == ns!());
+                //print!(" {}=\"{}\"", attr.name.local, attr.value);
+                if attr.name.local.eq_ignore_ascii_case(&atom!("src")) || 
+                   attr.name.local.eq_ignore_ascii_case(&atom!("href")) {
+                    //resource_list.push(replace(&mut attr.value.to_string(), String::new()));
+                    resource_list.push(attr.value.to_string());
+                }
+            }
+        }
+    }
+
+    for child in node.children.iter() {
+        walk(indent+4, child.clone(), &mut resource_list);
+    }
+
+
+}
+
+
+fn make_resource_list(url : &str, client : &Client) {
+    let mut response = match client.get(url).send(){
+        Err(why) => panic!("Couldn't get response: {}", why.description()),
+        Ok(response) => response,
+    };
+
+    let mut buf = String::new();
+    match response.read_to_string(&mut buf) {
+        Err(why) => panic!("Couldn't read response: {}", why.description()),
+        Ok(_) => (),
+    };
+
+    let dom = parse_document(RcDom::default(), Default::default()).one(buf);
+    let mut resource_list = vec!();
+    walk(0, dom.document, &mut resource_list);
+   
+  
+    let mut writer = match File::create("../resources.txt") {
+        Err(why) => panic!("Can't create resources.txt: {}", why.description()),
+        Ok(writer) => writer,
+    };
+
+    for r in resource_list.iter(){
+        write!(writer, "{}\n", r).expect("IO Error");
+    }
 
 }
 
